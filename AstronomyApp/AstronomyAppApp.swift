58bd6430d1c6da57e80aa8c0f88cf046509dc6f7
alimurad <rxalimurad@gmail.com>
2022-09-10 09:31:36 +0500

    Initial Commit

---
+++ AstronomyApp/AstronomyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyAppApp.swift
+//  AstronomyApp
+//
+//  Created by Ali Murad on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AstronomyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstronomyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AstronomyApp
+//
+//  Created by Ali Murad on 10/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
